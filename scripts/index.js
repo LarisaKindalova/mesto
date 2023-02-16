da98257c6c4b633161550768dfdfabd7fb71463c
Larisa Kindalova <l.kindalova@yandex.ru>
2023-02-16 18:50:43 +1000

    fix: исправить код согласно замечаниям ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 //popup
-let popupEditProfile = document.querySelector('.popup_edit-profile');
+let popupEditProfile = document.querySelector('.popup_type_edit-profile');
 
 //кнопки
 let profileAddButton = document.querySelector('.profile__add-button');
@@ -12,14 +12,12 @@ let popupCloseButton = document.querySelector('.popup__close-button');
 let popupEditForm = document.querySelector('.popup__edit-form');
 
 //поля формы редактирования
-let popupInputUsername = document.querySelector('.popup__input_username');
-let popupInputJob = document.querySelector('.popup__input_job');
-let popupSubmitButton = document.querySelector('.popup__submit-button');
+let popupInputUsername = document.querySelector('.popup__input_value_username');
+let popupInputJob = document.querySelector('.popup__input_value_job');
 
 //редкатировать профиль
 let profileUserName = document.querySelector('.profile__username');
 let profileJob = document.querySelector('.profile__job');
-console.log(popupInputUsername, popupInputJob, popupSubmitButton, profileUserName, profileJob);
 
 //открыть редактирование профиля
 profileEditButton.addEventListener('click', function() {
