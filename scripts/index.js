6ef7c02d373d47790ec405421112de1cfc77056b
Larisa Kindalova <l.kindalova@yandex.ru>
2023-02-27 12:25:43 +1000

    fix: исправить недочеты в стилях

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,41 @@
 //popup
-let popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const popupAddCard = document.querySelector('.popup_type_add-card');
 
-//кнопки редактирования
-let profileEditButton = document.querySelector('.profile__edit-button');
-
+//кнопки
+const profileEditButton = document.querySelector('.profile__edit-button');
+const
+profileAddButton = document.querySelector('.profile__add-button');
 // кнопка закрыть
-let popupCloseButton = document.querySelector('.popup__close-button');
+const popupCloseButton = document.querySelector('.popup__close-button');
 
 //форма редактирования
-let popupEditForm = document.querySelector('.popup__edit-form');
+const popupEditForm = document.querySelector('.popup__form_edit');
 
 //поля формы редактирования
-let popupInputUsername = document.querySelector('.popup__input_value_username');
-let popupInputJob = document.querySelector('.popup__input_value_job');
+const popupInputUsername = document.querySelector('.popup__input_value_username');
+const popupInputJob = document.querySelector('.popup__input_value_job');
 
 //редкатировать профиль
-let profileUserName = document.querySelector('.profile__username');
-let profileJob = document.querySelector('.profile__job');
+const profileUserName = document.querySelector('.profile__username');
+const profileJob = document.querySelector('.profile__job');
+
+//поля формы добавить фото
+const InputCardName = document.querySelector('.popup__input_value_card-name');
+const InputCardLink = document.querySelector('.popup__input_value_card-link');
 
 //открыть редактирование профиля
 profileEditButton.addEventListener('click', function() {
-  openPopup(popup);
+  openPopup(popupEditProfile);
   popupInputUsername.value = profileUserName.textContent;
   popupInputJob.value = profileJob.textContent;
+});
+
+//открытие формы добавить фото
+profileAddButton.addEventListener('click', function() {
+  openPopup(popupAddCard);
+
 });
 
 //функиця открытия popup
@@ -32,7 +45,7 @@ function openPopup(popup) {
 
 // закрыть popup
 popupCloseButton.addEventListener('click', function(){
-  popupClose(popup)
+  popupClose()
 });
 
 // функция закрытия popup
@@ -45,7 +58,7 @@ function handleFormSubmit (evt) {
   evt.preventDefault();
   profileUserName.textContent = popupInputUsername.value;
   profileJob.textContent = popupInputJob.value;
-  popupClose(popup);
+  popupClose(popupEditProfile);
 };
 
 popupEditForm.addEventListener('submit', handleFormSubmit);
