50fa39d681d87567553d3ef88dfd1178d57d8413
Larisa Kindalova <l.kindalova@yandex.ru>
2023-04-24 20:15:00 +1000

    feat: webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import './index.css';
 import { initialCards, profileEditButton, profileAddButton, inputUsername, inputJob, popupEditForm, popupAddForm } from '../utils/constants.js';
 import { validationConfig } from '../utils/constants.js';
 import { Card } from '../components/Card.js';
